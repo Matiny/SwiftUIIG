b572b89893048a81507dbdd9f43681a2ed9227b9
Matiny L <matiny@raves-air-2.lan>
2020-12-19 13:03:07 -0500

    Project started

---
+++ SwiftUIIG/SwiftUIIGApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIIGApp.swift
+//  SwiftUIIG
+//
+//  Created by Matiny L on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIIGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
